aa8a837f009b7f21328f7da991502e69aaccebc6
毛亚琛 <maoyachen55@gmail.com>
2023-11-30 22:55:47 +0800

    换成timer输出

--- src/main.c
+++ src/main.c
@@ -34,25 +34,40 @@ int main(void) {
     CS_enableClockRequest(CS_MCLK);
     CS_enableClockRequest(CS_SMCLK);
 
-    uint8_t data[32];
-    int n = 0;
-    ACCEL_result result;
-
-    for (;;) {
-        if (n >= 100) {
-            LED_toggleLED1();
-            n = 0;
-        }
-        ACCEL_singleSample(&result);
-        // repeat 5 times to make sure the data is sent
-        for (int i = 0; i < 5; i++) {
-            memcpy(data + i * sizeof(result), &result, sizeof(result));
-        }
-        BS_transmitFrame(data, 32);
-        __delay_cycles(160000);
-        n++;
-    }
+    // 初始化 TIMER
+    Timer_A_initUpModeParam initContParam = {0};
+    // 1 MHz
+    initContParam.clockSource = TIMER_A_CLOCKSOURCE_SMCLK;
+    // 1MHz / 20 = 50KHz
+    initContParam.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_20;
+    // 允许中断 CCR0 -> 0 时触发中断
+    initContParam.captureCompareInterruptEnable_CCR0_CCIE = TIMER_A_CCIE_CCR0_INTERRUPT_ENABLE;
+    initContParam.timerClear = TIMER_A_DO_CLEAR;
+    initContParam.startTimer = true;
+    // 20ms 触发一次中断
+    initContParam.timerPeriod = 50000 / 50;
+    Timer_A_initUpMode(TIMER_A0_BASE, &initContParam);
 
     __bis_SR_register(GIE);
     return 0;
+}
+
+int n = 0;
+
+// 50Hz 中断
+void __attribute__((interrupt(TIMER0_A0_VECTOR))) Timer_A0_ISR(void) {
+    uint8_t data[32];
+    ACCEL_result result;
+    // 每两秒闪烁一次 LED1
+    if (n >= 49) {
+        LED_toggleLED1();
+        n = 0;
+    }
+    ACCEL_singleSample(&result);
+    for (int i = 0; i < 5; i++) {
+        memcpy(data + i * sizeof(result), &result, sizeof(result));
+    }
+    BS_transmitFrame(data, 32);
+    n++;
+    Timer_A_clearCaptureCompareInterrupt(TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
 }
